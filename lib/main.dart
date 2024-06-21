84f181823dbb6e044644cedc5637620e15632fe8
JF-21 <j.friedel@qsl.eu.com>
2024-06-21 14:49:35 +0200

    Login Page done :)

--- lib/main.dart
+++ lib/main.dart
@@ -10,36 +10,80 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  const MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 0, 242, 218),
+        backgroundColor: Colors.grey,
         body: SafeArea(
-          child: Column(
+          child:  Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
-                backgroundImage: AssetImage('images/12.webp'),
+                backgroundImage: AssetImage('images/2019_09_Neues_Lager.jpg'),
               ),
               Text(
-                'JF-21',
+                'Login',
                 style: TextStyle(
                   fontFamily: 'Playwrite',
                   fontSize: 30,
-                  color: Colors.white,
+                  color: Colors.black,
                   fontWeight: FontWeight.bold
                 ),
               ),
               Text(
-                'The Developer',
+                'Lager-Login',
                 style: TextStyle(
-                  height: 2,
-                  fontFamily: 'Playwrite',
+                  height: 3,
                   fontSize: 20,
-                  color: Colors.grey,
+                  color: Color.fromARGB(255, 255, 0, 0),
                   letterSpacing: 2.0,
                   fontWeight: FontWeight.bold
                 ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.black,
+                ),
+                ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 700.0),
+                child: ListTile(
+                  leading: Icon(
+                        Icons.supervised_user_circle,
+                        color: Colors.red,
+                        size: 25,
+                        ),
+                        title:  Text(
+                        'Username', 
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        ),
+                        ) ,
+                )
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 700.0),
+                child: ListTile(
+                  leading: Icon(
+                        Icons.key,
+                        color: Colors.red,
+                        size: 25,
+                        ),
+                        title:  Text(
+                        'Password', 
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        ),
+                        ) ,
+                )
               )
+             
             ],
            ),
         ),
@@ -47,3 +91,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }  
+
