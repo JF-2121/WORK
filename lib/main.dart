6bc33d886e1d97446f8ca1777649cbdeb1754a03
JF-21 <j.friedel@qsl.eu.com>
2024-06-21 10:16:37 +0200

    Jaman first steps :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Center(child: Text('Hello World'))));
+void main (){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 80, 80, 80),
+        appBar: AppBar(
+          title: const Text('@Meh'),
+          backgroundColor: Colors.redAccent,
+          ),
+          body:  const Center(
+            child: Image(
+              image: AssetImage('images/diamond.png')
+            ),
+          ),
+      )
+    )
+  );
+} 
