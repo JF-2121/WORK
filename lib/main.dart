a6676446ee3284d03e3c7594b4f5b7edcbd23364
JF-21 <j.friedel@qsl.eu.com>
2024-06-21 12:21:45 +0200

    weiter mit dem grind

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,58 @@ import 'package:flutter/material.dart';
 
 void main (){
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 80, 80, 80),
-        appBar: AppBar(
-          title: const Text('@Meh'),
-          backgroundColor: Colors.redAccent,
-          ),
-          body:  const Center(
-            child: Image(
-              image: AssetImage('images/diamond.png')
-            ),
-          ),
-      )
-    )
+    const MyApp()
   );
 } 
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [ 
+            Container(            
+            width: 100.0,
+            color: Colors.red,
+            ),
+            
+            const SizedBox(
+              width: 100,
+            ),
+          
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.yellow,
+            ),
+           
+        
+            Container(
+              margin: const EdgeInsetsDirectional.fromSTEB(0, 200, 0, 0),
+              width: 100.0,
+              height: 100.0,
+              color: Colors.green,
+            ),
+
+            const SizedBox(
+              width: 100,
+            ),
+          
+            Container(            
+            width: 100.0,
+            color: Colors.blue,
+            ),
+
+            
+          ],
+          ),
+        ),
+      ),
+    );
+  }
+}  
