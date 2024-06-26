00c26fefce7010857b95b8b5576a19404976a191
JF-21 <j.friedel@qsl.eu.com>
2024-06-26 14:33:14 +0200

    weiter mit dem grind

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
- 
+import 'dart:math';
 
 void main (){
   return runApp(
@@ -7,16 +7,31 @@ void main (){
       home: Scaffold(
       backgroundColor: Colors.red,
       appBar: AppBar(
-        title: Text('Dice'),
+        title: const Text('Dice'),
         backgroundColor:  Colors.grey,
       ),
-      body: DicePage(),
+      body: const DicePage(),
     ),
   )
   );
 } 
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
 
-class DicePage extends StatelessWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1; 
+  int rightDiceNumber = 1; 
+
+  void changeDiceFace() {
+    setState(() {
+        leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,37 +39,40 @@ class DicePage extends StatelessWidget {
         children: [ 
           Expanded(
             child: Padding(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: OutlinedButton(
                 style: OutlinedButton.styleFrom(
-                  padding: EdgeInsets.all(0.1),
-                  side: BorderSide(width: 0.1, color: Colors.transparent), // Custom border width and color
+                  padding: const EdgeInsets.all(0.1),
+                  side: const BorderSide(width: 0.1, color: Colors.transparent), // Custom border width and color
                 ),
                 iconAlignment: IconAlignment.end,
                 onPressed: () {
-                  debugPrint('Recieved click left');
-                } ,
-                child: Image.asset('images/dice1.png')),
+                  setState(() {
+                    changeDiceFace();
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: OutlinedButton(
                 style: OutlinedButton.styleFrom(
-                  padding: EdgeInsets.all(0.1),
-                  side: BorderSide(width: 0.1, color: Colors.transparent), // Custom border width and color
+                  padding: const EdgeInsets.all(0.1),
+                  side: const BorderSide(width: 0.1, color: Colors.transparent), // Custom border width and color
                 ),
                 iconAlignment: IconAlignment.end,
                 onPressed: () {
-                  debugPrint('Recieved click right');
-                } ,
-                child: Image.asset('images/dice1.png')),
-       ),
+                  setState(() {
+                    changeDiceFace();
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+             ),
           ),
         ],
       ),
     );
   }
 }
-
