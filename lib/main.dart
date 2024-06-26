bb63865666a6ee0445cf68239feab508841ec000
JF-21 <j.friedel@qsl.eu.com>
2024-06-26 14:38:53 +0200

    heute nicht mehr soviel weitermachen den mist eher zuhause ong

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main (){
   return runApp(
@@ -10,69 +9,7 @@ void main (){
         title: const Text('Dice'),
         backgroundColor:  Colors.grey,
       ),
-      body: const DicePage(),
     ),
   )
   );
 } 
-class DicePage extends StatefulWidget {
-  const DicePage({super.key});
-
-  @override
-  State<DicePage> createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1; 
-  int rightDiceNumber = 1; 
-
-  void changeDiceFace() {
-    setState(() {
-        leftDiceNumber = Random().nextInt(6) + 1;
-        rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [ 
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  padding: const EdgeInsets.all(0.1),
-                  side: const BorderSide(width: 0.1, color: Colors.transparent), // Custom border width and color
-                ),
-                iconAlignment: IconAlignment.end,
-                onPressed: () {
-                  setState(() {
-                    changeDiceFace();
-                  });
-                },
-                child: Image.asset('images/dice$leftDiceNumber.png')),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  padding: const EdgeInsets.all(0.1),
-                  side: const BorderSide(width: 0.1, color: Colors.transparent), // Custom border width and color
-                ),
-                iconAlignment: IconAlignment.end,
-                onPressed: () {
-                  setState(() {
-                    changeDiceFace();
-                  });
-                },
-                child: Image.asset('images/dice$rightDiceNumber.png')),
-             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
