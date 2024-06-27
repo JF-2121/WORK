0cdd6292bf6d4af4b1f22b81a4b861041b5e3363
JF-21 <j.friedel@qsl.eu.com>
2024-06-27 09:59:18 +0200

    man wird besser :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,125 @@
-import 'package:audioplayers/audioplayers.dart';
+import 'dart:collection';
+import 'questions.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
+void main() => runApp(Quizzler());
 
-//does not work on the c laptop due to not high enough sec lvl
-void main() => runApp(XylophoneApp());
-
-class XylophoneApp extends StatelessWidget {
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
         body: SafeArea(
-          child: Center(
-            child: OutlinedButton(
-              onPressed: (){
-                final player = AudioCache();
-              }, child: Text('ClickMe'),
-              )),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
         ),
       ),
     );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+ 
+
+  List<Questions> questionBank = [
+    Questions('A slug\'s blood is green', false),
+    Questions('Fok', true),
+    Questions('scheisa', true),
+  ];
+
+  int questionNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+         Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questionBank[questionNumber].questionsText,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: OutlinedButton(
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+
+                bool correctAnswers = questionBank[questionNumber].questionsAnswer;
+
+                if(correctAnswers == true){
+                  print('usergotitright');
+                } else {
+                  print('usergotitwrong');
+                }
+
+                setState(() {
+                  questionNumber++;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: OutlinedButton(
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                bool correctAnswers = questionBank[questionNumber].questionsAnswer;
+
+                if(correctAnswers == false){
+                  print('usergotitright');
+                } else {
+                  print('usergotitwrong');
+                }
+
+                setState(() {
+                  questionNumber++;
+                });              
+                },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ) //TODO: Add a Row here as your score keeper
+      ],
+    );
+  }
+}
+
